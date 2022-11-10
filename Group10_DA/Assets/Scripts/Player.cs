4653e428ab2e3437e51fd228bf66056ec0332674
17-WynnOng <114891238+17-WynnOng@users.noreply.github.com>
2022-11-10 12:47:30 +0800

    Build

--- Group10_DA/Assets/Scripts/Player.cs
+++ Group10_DA/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    public float damage;
+    public float damage = 10f;
     // Start is called before the first frame update
     void Start()
     {
