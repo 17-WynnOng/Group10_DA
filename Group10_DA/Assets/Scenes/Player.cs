8b380b9f7f2fa460d792ff36fb73502daac2749a
17-WynnOng <114891238+17-WynnOng@users.noreply.github.com>
2022-11-10 11:00:18 +0800

    Player Movement

---
+++ Group10_DA/Assets/Scenes/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if(transform.position.x < -7.55f)
+        {
+            this.transform.position = new Vector3(-7.55f, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x > 20.322f)
+        {
+            this.transform.position = new Vector3(20.322f, transform.position.y, transform.position.z);
+        }
+    }
+}
