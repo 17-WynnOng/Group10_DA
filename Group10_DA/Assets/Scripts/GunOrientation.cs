31a6751993da6a9b533ccc43b810899de24ea32e
17-WynnOng <114891238+17-WynnOng@users.noreply.github.com>
2022-11-14 17:18:46 +0800

    Weapon Orientation

---
+++ Group10_DA/Assets/Scripts/GunOrientation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunOrientation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateTowardsTarget();
+    }
+
+    private void RotateTowardsTarget()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            RotateTowardsMouse(hit.point);
+        }
+
+    }
+
+    private void RotateTowardsMouse(Vector3 TargetPosition)
+    {
+        Vector3 DirectionVector = TargetPosition - this.transform.position;
+
+        this.transform.forward = DirectionVector;
+    }
+
+
+}
