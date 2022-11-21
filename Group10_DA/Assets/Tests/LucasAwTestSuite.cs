b6f095f2677f99603da1993ca8c81b0f6a552acd
16-LucasAw <RealAkaliMainer@gmail.com>
2022-11-21 11:06:24 +0800

    Created LucasAwTestSuite

---
+++ Group10_DA/Assets/Tests/LucasAwTestSuite.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LucasAwTestSuite
+    {
+
+        public bool normalrobotmovement = true;
+        public bool heavyrobotmovement = true;
+        public bool normalrobotpunch = true;
+        public bool heavyrobotpunch = true;
+        
+        [Test]
+        public void NormalRobotMovementSFX()
+        {
+            Assert.AreEqual(normalrobotmovement, true);
+        }
+
+        [Test]
+        public void HeavyRobotMovementSFX()
+        {
+            Assert.AreEqual(heavyrobotmovement, true);
+        }
+
+        [Test]
+        public void NormalRobotPunchSFX()
+        {
+            Assert.AreEqual(normalrobotpunch, true);
+        }
+
+        [Test]
+        public void HeavyRobotPunchSFX()
+        {
+            Assert.AreEqual(heavyrobotpunch, true);
+        }
+    }
+}
